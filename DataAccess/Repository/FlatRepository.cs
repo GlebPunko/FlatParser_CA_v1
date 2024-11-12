11155eb134fcffa72df2e28da6d21086cf689adc
Hleb Punko <hleb.punko01@gmail.com>
2024-11-12 18:20:44 +0300

    Add repositories

---
+++ DataAccess/Repository/FlatRepository.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Entity;
+
+namespace DataAccess.Repository
+{
+    public class FlatRepository(string connectionString) : MsSqlRepositoryBase(connectionString)
+    {
+        public async Task<IEnumerable<FlatEntity>> GetFlats(CancellationToken cancellationToken = default)
+        {
+            var sql = "SELECT * FROM [dbo].Flats;";
+
+            return await Database.LoadData<FlatEntity>(sql, parameters: null, cancellationToken);
+        }
+
+        public async Task<bool> AddFlat(FlatEntity flat)
+        {
+            var sql = "INSERT INTO [dbo].Flats (Link, Address, Price) VALUES (@Link, @Address, @Price)";
+
+            return await Database.SaveData(sql, new {flat.Link, flat.Address, flat.Price});
+        }
+    }
+}
---
+++ DataAccess/Repository/MsSQLRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using Database;
+
+namespace DataAccess.Repository
+{
+    public abstract class MsSqlRepositoryBase
+    {
+        protected IDb Database { get; }
+
+        public MsSqlRepositoryBase(string connectionString)
+        {
+            Database = new Database.MsSql(connectionString);
+        }
+    }
+}
